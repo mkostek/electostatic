594f0fded2c483250bd104432b1340ec595d81ea
mustafa kostek <mustafakostek@gmail.com>
2017-02-09 22:35:11 +0300

    Add files via upload

---
+++ elips_buton_olusturma/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace elips_buton_olusturma
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chartBirinciDerece = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.shapedButton1 = new elips_buton_olusturma.ShapedButton();
+			this.shapedButton2 = new elips_buton_olusturma.ShapedButton();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chartBirinciDerece)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chartBirinciDerece
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chartBirinciDerece.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chartBirinciDerece.Legends.Add(legend1);
+			this.chartBirinciDerece.Location = new System.Drawing.Point(12, 13);
+			this.chartBirinciDerece.Name = "chartBirinciDerece";
+			this.chartBirinciDerece.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+			series1.BorderWidth = 3;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Color = System.Drawing.Color.Red;
+			series1.Legend = "Legend1";
+			series1.Name = "birinci yük";
+			series2.BorderWidth = 3;
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series2.Color = System.Drawing.Color.DarkBlue;
+			series2.Legend = "Legend1";
+			series2.Name = "ikinci yük";
+			this.chartBirinciDerece.Series.Add(series1);
+			this.chartBirinciDerece.Series.Add(series2);
+			this.chartBirinciDerece.Size = new System.Drawing.Size(357, 313);
+			this.chartBirinciDerece.TabIndex = 0;
+			this.chartBirinciDerece.Text = "chart1";
+			// 
+			// shapedButton1
+			// 
+			this.shapedButton1.BackColor = System.Drawing.SystemColors.GrayText;
+			this.shapedButton1.Location = new System.Drawing.Point(637, 157);
+			this.shapedButton1.Name = "shapedButton1";
+			this.shapedButton1.Size = new System.Drawing.Size(183, 190);
+			this.shapedButton1.TabIndex = 0;
+			this.shapedButton1.Text = "shapedButton1";
+			this.shapedButton1.UseVisualStyleBackColor = false;
+			this.shapedButton1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Down);
+			this.shapedButton1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Move);
+			this.shapedButton1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Up);
+			// 
+			// shapedButton2
+			// 
+			this.shapedButton2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.shapedButton2.Location = new System.Drawing.Point(401, 163);
+			this.shapedButton2.Name = "shapedButton2";
+			this.shapedButton2.Size = new System.Drawing.Size(178, 179);
+			this.shapedButton2.TabIndex = 1;
+			this.shapedButton2.Text = "shapedButton2";
+			this.shapedButton2.UseVisualStyleBackColor = false;
+			this.shapedButton2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Down);
+			this.shapedButton2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Move);
+			this.shapedButton2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Up);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"mavi",
+									"gri"});
+			this.comboBox1.Location = new System.Drawing.Point(500, 18);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 22);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(500, 44);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(500, 70);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(422, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 23);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "küreseç";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(422, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "yarıçap";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(422, 70);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(63, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "yükü";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(624, 70);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "güncelle";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// chart1
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea2);
+			legend2.Name = "Legend1";
+			this.chart1.Legends.Add(legend2);
+			this.chart1.Location = new System.Drawing.Point(12, 350);
+			this.chart1.Name = "chart1";
+			this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+			series3.BorderWidth = 4;
+			series3.ChartArea = "ChartArea1";
+			series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			series3.Legend = "Legend1";
+			series3.Name = "itme çekme";
+			series3.YValuesPerPoint = 2;
+			this.chart1.Series.Add(series3);
+			this.chart1.Size = new System.Drawing.Size(376, 208);
+			this.chart1.TabIndex = 8;
+			this.chart1.Text = "chart1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1306, 570);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.shapedButton2);
+			this.Controls.Add(this.shapedButton1);
+			this.Controls.Add(this.chartBirinciDerece);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.chartBirinciDerece)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chartBirinciDerece;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private elips_buton_olusturma.ShapedButton shapedButton2;
+		private elips_buton_olusturma.ShapedButton shapedButton1;
+
+		#endregion
+	}
+}
+
---
+++ elips_buton_olusturma/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace elips_buton_olusturma
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			
+		}
+		public struct iletken
+		{
+			public double yuk;
+			public double yarıcap;
+		}
+		iletken a,b;
+		double r=0.5;
+		bool suruklenmedurumu = false; //Class seviyesinde bir field(değişken) tanımlıyoruz ki,MouseDown da biz bunu true yapacağız,MouseUpta false değerini alacak ve MouseMove eventında true ise hareket edecek.
+		Point ilkkonum; //Global bir değişken tanımlıyoruz ki, ilk tıkladığımız andaki konumunu çıkarmadığımızda buton mouse imlecinden daha aşağıdan hareket edecektir.
+		private void Down(object sender, MouseEventArgs e)
+		{
+			suruklenmedurumu = true; //işlemi burada true diyerek başlatıyoruz.
+			((ShapedButton)sender).Cursor = Cursors.SizeAll; //SizeAll yapmamımızın amacı taşırken hoş görüntü vermek için
+			ilkkonum = e.Location; //İlk konuma gördüğünüz gibi değerimizi atıyoruz.
+			
+		}
+		private void Move(object sender, MouseEventArgs e)
+		{
+			if (suruklenmedurumu) // suruklenmedurumu==true ile aynı.
+			{
+				((ShapedButton)sender).Left = e.X + ((ShapedButton)sender).Left - (ilkkonum.X);
+				// button.left ile soldan uzaklığını ayarlıyoruz. Yani e.X dediğimizde buton üzerinde mouseun hareket ettiği pixeli alacağız + butonun soldan uzaklığını ekliyoruz son olarakta ilk mouseın tıklandığı alanı çıkarıyoruz yoksa butonun en solunda olur mouse imleci. Alttakide aynı şekilde Y koordinati için geçerli
+				((ShapedButton)sender).Top = e.Y + ((ShapedButton)sender).Top - (ilkkonum.Y);
+				chart1.Series[0].Points.Add(kuvvet());
+			}
+			
+		}
+		private void Up(object sender, MouseEventArgs e)
+		{
+			suruklenmedurumu = false; //Sol tuştan elimizi çektik artık yani sürükle işlemi bitti.
+			((ShapedButton)sender).Cursor = Cursors.Default; //İmlecimiz(Cursor) default değerini alıyor.
+		}
+		public double denge()
+		{
+			return (a.yuk+b.yuk)/(a.yarıcap+b.yarıcap);
+		}
+		public int mesafe()
+		{
+			return	(int)Math.Sqrt(Math.Pow(shapedButton1.Location.X-shapedButton2.Location.X,2)+Math.Pow(shapedButton2.Location.Y-shapedButton1.Location.Y,2));
+		}
+		public int kuvvet()
+		{
+			return (int)(9*(Math.Pow(10,9)*(a.yuk*b.yuk)/Math.Pow(mesafe(),2)));
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{/*
+            ShapedButton elips_Buton = new ShapedButton();
+            elips_Buton.Location = new Point(120, 60);
+            elips_Buton.FlatAppearance.BorderSize = 0;
+            elips_Buton.BackColor = Color.Blue;
+            elips_Buton.FlatStyle = FlatStyle.Flat;
+            this.Controls.Add(elips_Buton);*/
+			timer1.Start();
+		}
+		public void hesaplama()
+		{
+			
+			if(a.yuk>denge()*a.yarıcap)
+			{
+				a.yuk-=r;b.yuk+=r;
+			}
+			else if(a.yuk<denge()*a.yarıcap)
+			{
+				a.yuk+=r;b.yuk-=r;
+
+			}
+			shapedButton2.Text="yük:"+a.yuk+"\nyarıçap:"+a.yarıcap;
+			chartBirinciDerece.Series[0].Points.Add(a.yuk);
+			shapedButton1.Text="yük:"+b.yuk+"\nyarıçap:"+b.yarıcap;
+			chartBirinciDerece.Series[1].Points.Add(b.yuk);
+			chart1.Series[0].Points.Add(kuvvet());
+
+		}
+
+		public bool kontrol(Button b1,Button b2)
+		{
+			int a,b,c,d;
+			a=(b1.Right-b2.Left)*(b2.Bottom-b1.Top);
+			b=(b1.Bottom-b2.Top)*(b1.Right-b2.Left);
+			c=(b1.Bottom-b2.Top)*(b2.Right-b1.Left);
+			d=(b2.Bottom-b1.Top)*(b2.Right-b1.Left);
+			if(a>0&&b>0&&c>0&&d>0)
+				return true;
+			else return false;
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			if(kontrol(shapedButton1,shapedButton2)){
+				hesaplama();
+			}
+			
+		}
+
+		void Button1Click(object sender, EventArgs e)
+		{
+			switch(comboBox1.SelectedIndex){
+					case 0:a.yarıcap=Convert.ToDouble(textBox1.Text);
+					a.yuk=Convert.ToDouble(textBox2.Text);
+					shapedButton2.Width=(int)a.yarıcap*50;
+					shapedButton2.Height=(int)a.yarıcap*50;
+					shapedButton2.Text="yük:"+a.yuk+"\nyarıçap:"+a.yarıcap;
+					break;
+					case 1:b.yarıcap=Convert.ToDouble(textBox1.Text);
+					b.yuk=Convert.ToDouble(textBox2.Text);
+					shapedButton1.Width=(int)b.yarıcap*50;
+					shapedButton1.Height=(int)b.yarıcap*50;
+					shapedButton1.Text="yük:"+b.yuk+"\nyarıçap:"+b.yarıcap;
+					break;
+					default:break;
+			}
+			chartBirinciDerece.Series[0].Points.Clear();
+			chartBirinciDerece.Series[1].Points.Clear();
+			
+			chart1.Series[0].Points.Clear();
+		}
+	}
+	public class ShapedButton : Button //Buton sınıfından miras alınıyor
+	{
+		// Elips buton a yeni bir boyut  verildiğinde şeklini elips yapıyor
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+			GraphicsPath gp = new GraphicsPath();
+			// this.Size ın en boyu aynı oldugundan elips bir şekil oluşuyor
+			gp.AddEllipse(new Rectangle(Point.Empty, this.Size));
+			// oluşan yeni elips şekli oluşturulan butona atanıyor
+			this.Region = new Region(gp);
+			// Butonun yeni şekli elips oluyor
+		}
+	}
+}
